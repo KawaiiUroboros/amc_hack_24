d428fff2574056756756deb1940f2aa220b3ae5d
kawaiiuroboros <kawaiiuroboros@gmail.com>
2021-04-25 17:00:17 +0300

    mew

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import { Text, View, StyleSheet, Animated } from 'react-native';
+import { Text, View, StyleSheet, Animated,FlatList, TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
 import * as React from 'react';
 import { useState, useEffect,Button } from 'react';
@@ -8,6 +8,7 @@ import {ExpoScanner} from './ExpoScanner'
 import { BarCodeScanner } from 'expo-barcode-scanner';
 export default function App() {
   const [t, sett] = useState(false);
+  const [s, sets] = useState(false);
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -21,7 +22,8 @@ export default function App() {
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`You have ${data} minute`);
-    sett(true);
+    sets(true);
+    
   };
 
   if (hasPermission === null) {
@@ -32,7 +34,7 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      {t&&(<CountdownCircleTimer
+      {t&&!s&&(<CountdownCircleTimer
         isPlaying
         duration={60}
         colors={[
@@ -47,7 +49,7 @@ export default function App() {
         </Animated.Text>
       )}
     </CountdownCircleTimer>)}{
-      !t&&( 
+      !t&&!s&&( 
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
@@ -55,7 +57,16 @@ export default function App() {
         
      )
     }
-{scanned &&!t&& <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+{scanned &&!t&&!s &&<Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}{s&&(
+<FlatList
+
+          data={[
+            { key: 'Free slot on 13:00' },
+            { key: '13:30 is busy' },
+
+          ]}
+          renderItem={({ item }) => <TouchableOpacity onPress={(f)=>{alert("You took a slot on 13 00"); sett(true);sets(false)}}><Text style={styles.item}>{item.key}</Text></TouchableOpacity>}
+        />)}
   </View>
   )
 }
